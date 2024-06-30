13b1aab390e0e81801c67f3a3db7c20ec503ec6a
HaiderAli99888 <122993240+HaiderAli99888@users.noreply.github.com>
2024-06-30 02:30:31 -0400

    Db initializer added for online publishing

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Bulky.Utility;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Stripe;
+using Bulky.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,6 +39,8 @@ builder.Services.AddSession(options => {
     options.Cookie.IsEssential = true;
 });
 
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
 
 builder.Services.AddRazorPages(); 
 builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
@@ -60,6 +63,8 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseSession();
+SeedDatabase();
+
 app.MapRazorPages();
 
 app.MapControllerRoute(
@@ -67,3 +72,13 @@ app.MapControllerRoute(
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
